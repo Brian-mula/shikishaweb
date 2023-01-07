934d9e111b4ce4787f5e1a21604281480d58ca6f
Brian-mula <mulatiwangilabrian@gmail.com>
2023-01-08 00:52:14 +0300

    layputs

---
+++ components/homeNav.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+      <nav class="flex items-center justify-between flex-wrap bg-white p-6">
+        <div class="flex items-center flex-shrink-0 text-gray-600 mr-6">
+          <img
+            src="~assets/images/shikishika_logo.svg"
+            class="h-8 w-8 rounded-full"
+            alt=""
+          />
+          <span class="font-semibold text-md tracking-tight">SHIKISHA</span>
+        </div>
+        <div class="block lg:hidden">
+          <button
+            class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
+          >
+            <svg
+              class="fill-current h-3 w-3"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+          </button>
+        </div>
+        <div
+          class="w-full flex flex-grow lg:flex lg:items-center lg:w-auto lg:justify-end"
+        >
+        
+          <div class="mr-auto">
+            <button
+              type="button"
+              class="inline-block text-sm px-4 py-2 leading-none border rounded bg-orange-500 text-white border-white hover:border-transparent hover:text-white hover:bg-orange-600 mt-4 lg:mt-0"
+              >Logout</button
+            >
+          </div>
+        </div>
+      </nav>
+    </div>
+  </template>
+  
+  <script setup></script>
+  
+  <style lang="scss" scoped></style>
+  
---
+++ layouts/normalLay.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="flex">
+        <div class="w-60">
+            <h3>Side Bar</h3>
+        </div>
+        <div class="">
+            <HomeNav/>
+            <slot/>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
